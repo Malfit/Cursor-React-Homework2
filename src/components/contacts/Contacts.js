5b2c3f7fc22673f9a822de969d7f09eb6f16b7ec
Malfit <oleksandr.rybakov@nure.ua>
2020-04-07 04:13:16 +0300

    phone book

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,28 +1,82 @@
 import React, {Component} from 'react';
+import Contact from '../contact';
+import SearchPanel from '../search-panel'
+import './Contacts.css';
 
 export default class Contacts extends Component{
+    state = {
+        contacts : [{
+          firstName: "Барней",
+          lastName: "Стинсовский",
+          phone: "380956319521",
+          gender: "male"
+      }, {
+          firstName: "Робин",
+          lastName: "Щербатская",
+          phone: "380931460123",
+          gender: "female"
+      }, {
+          firstName: "Аномный",
+          lastName: "Анонимус",
+          phone: "380666666666"
+      }, {
+          firstName: "Лилия",
+          lastName: "Олдровна",
+          phone: "380504691254",
+          gender: "female"
+      }, {
+          firstName: "Маршэн",
+          lastName: "Эриксонян",
+          phone: "380739432123",
+          gender: "male"
+      }, {
+          firstName: "Теодор",
+          lastName: "Мотсбэс",
+          phone: "380956319521",
+          gender: "male"
+        }],
+        search : "", 
+      }
 
-    ourContacts =  this.contatsProp.map((item)=>{
-        return (
-            <li>
-                    this.firstName = {item.firstName}
-                    this.lastName = {item.lastName}
-                    this.phone = {item.phone}
-                    this.gender = {item.gender}
-            </li>
-        )
-    })   
+    onSearchChange = (search) =>{
+        this.setState({search})
+    };
 
+    onCheckBoxChange = (checkM)=>{
+        this.setState({checkM})
+    };
+    
+    search(items,search){
 
+        if (search.length === 0){
+            return items;
+        } else {
+            return items.filter((item) => {
+                return item.firstName.toLowerCase().indexOf(search.toLowerCase()) > -1 
+                || item.phone.indexOf(search) > -1 ;
 
-    render(){
-        return (
-            <ul>
-            {this.ourContacts}
-        </ul>
-        )
-
+            })
+        }
+    }
+   
+    render(){  
+        const {contacts, search} = this.state;
+        const visibleItems = this.search(contacts, search);
+        const contact = visibleItems.map((item) => {    
+            return (
+                <div className = "allContacts">      
+                    {console.log(item)}             
+                    <Contact {...item}/> 
+                </div>
+            )
+        })
         
+        return (
+            <div>
+              <SearchPanel onSearchChange = {this.onSearchChange}/>
+              {contact}
+            </div>
+        )        
     }
 }
 
