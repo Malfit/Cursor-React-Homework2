46f3b5f704e6d1f9bcfe68b4821d1149fface13b
Malfit <oleksandr.rybakov@nure.ua>
2020-04-12 20:03:51 +0300

    Updates

---
+++ precache-manifest.ff3966e9c8705a83ad3222fa914a3aa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40de420857b5412aeed5baac6de7b109",
+    "url": "/Cursor-React-Homework2/index.html"
+  },
+  {
+    "revision": "490e1fdfb544c87e5a43",
+    "url": "/Cursor-React-Homework2/static/css/main.4758dd35.chunk.css"
+  },
+  {
+    "revision": "3db7120e92182e8416c2",
+    "url": "/Cursor-React-Homework2/static/js/2.46cad634.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cursor-React-Homework2/static/js/2.46cad634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490e1fdfb544c87e5a43",
+    "url": "/Cursor-React-Homework2/static/js/main.8464bd2b.chunk.js"
+  },
+  {
+    "revision": "b9728d801d0071ffe867",
+    "url": "/Cursor-React-Homework2/static/js/runtime-main.c85ed79e.js"
+  }
+]);
